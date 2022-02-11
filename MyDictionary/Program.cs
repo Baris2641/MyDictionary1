f6a947dddb27dd85f3ba74958531bb80288b66b8
BARIŞ IŞIK <20202305034@ogr.akdeniz.edu.tr>
2022-02-11 16:36:59 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> isimler = new MyDictionary<string>();
+            isimler.Add("Engin");
+        }
+    }
+}
